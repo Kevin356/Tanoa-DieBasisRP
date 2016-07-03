4e68cb55a395226c9ea55f796abce174ebdab106
Psychou <Green97@gmx.net>
2016-07-03 17:47:02 +0200

    Intro Entfernt

--- Altis_Life.Altis/Functions.hpp
+++ Altis_Life.Altis/Functions.hpp
@@ -211,15 +211,6 @@ class Life_Client_Core
 		class nearestObjects {};
 		class loggedOut {};
 	};
-<<<<<<< HEAD
-	
-	 class Basis
-	{
-		file = "core\basis";
-		class initIntro {};
-	};
-=======
->>>>>>> parent of 3890dfe... Neues Intro
 
 	class Network
 	{
